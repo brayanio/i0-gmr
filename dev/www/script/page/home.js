7992479a2dc3d65ac91abd25a38bf6299faf5921
Todd Lewis aka Brayan <brayanbyrdsong@icloud.com>
2022-05-04 12:32:30 -0600

    logos

--- dev/www/script/page/home.js
+++ dev/www/script/page/home.js
@@ -3,19 +3,24 @@
     ${Service.Util.Nav}
     <br>
     <div class="container">
-        <section class="card">
-            <div class="card-header">
-                <h1 class="card-title tls-font">gmr</h1>
-                <h5 class="card-subtitle mb-0 text-muted tls-font">engine</h5>
-            </div>
-            <div class="card-body">
-                <p class="card-text" style="max-width: 369px">The quickest game engine ever made by a single man in his basement.</p>
-                <div class="d-flex align-items-center">
-                    <a href="#download" class="card-link text-decoration-none">Download</a>
-                    <a href="#documentation" class="card-link btn btn-outline-primary text-decoration-none">Documentation</a>
+        <div class="row justify-content-md-center justify-content-lg-center">
+            <div class="col-md-auto col-lg-auto col-md-6 col-lg-4">
+                <div class="card">
+                    <div class="card-header d-flex justify-content-center">
+                        <img src="./asset/logo.PNG" width="128" height="128">
+                    </div>
+                    <div class="card-body">
+                        <h1 class="card-title tls-font">gmr</h1>
+                        <h5 class="card-subtitle text-muted tls-font mb-2">engine</h5>
+                        <p class="card-text text-center">The JavaScript game engine solution.</p>
+                        <div class="d-flex align-items-center">
+                            <a href="#download" class="card-link text-decoration-none">Download</a>
+                            <a href="#documentation" class="card-link btn btn-outline-primary text-decoration-none">Documentation</a>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </section>
+        </div>
     </div>
 `,
 async (ui, props) => {
